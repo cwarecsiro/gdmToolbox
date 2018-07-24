52d3b19ae3f1bbe53562806ef8073153f2e993f5
Ware, Chris (L&W, Black Mountain) <Chris.Ware@csiro.au>
2018-07-24 12:59:29 +1000

    init files

---
+++ R/variable_select.R
@@ -0,0 +1,129 @@
+#'@title GDM variable selection
+#'
+#'@description Perform backward elimination to determine variable significance in a gdm 
+#'
+#'@param data (string, data.frame or matrix). Input table, can be in memory (data.frame or matrix) or filepath to .CSV
+#'@param dst (string) Filepath to write outputs to. A subdir (taking the name of outname below) will be created here.
+#'@param outname (string) Filename to use for writing outputs. Default: 'gdm_variable_selection'
+#'@param permutations (int) Number of permutations to run for each variable. Default: 100.
+#'@param geo (boolean) Whether or not to include geographic distance as a predictor. Default: TRUE
+#'@param load_output (boolean) If TRUE (default) output files will be loaded into memory in a list.
+#'@param verbose (boolean) Print out messages to console. Default: FALSE
+#'
+#'@return Files written to dst.
+#'
+#'@examples gdm.variable_select(inputTable, dst_dir, analysis_1, 50, TRUE, FALSE, TRUE)
+#'
+#'@importFrom gdm gdm
+#'@importFrom assertthat assert_that
+#'@importFrom data.table fread
+#'
+#'@export
+
+gdm.variable_select = function(data, dst, outname = 'gdm_variable_selection',
+                               permutations = 100, geo = TRUE, load_output = TRUE,
+                               verbose = FALSE){
+  
+  ## check what data is
+  data_class = class(data)
+  in_memory = sum(unlist(lapply(c('data.frame', 'matrix', 'data.table')), 
+                         function(x) grep(x, data_class)))
+  if(in_memory > 0){
+    ## in memory is true
+    data = data.frame(data)
+    numpreds = (ncol(data) - 6) / 2
+    ## write to tmp
+    tmp_loc = tempfile(fileext = '.csv')
+    write.csv(data, tmp_loc, row.names = FALSE)
+    data = tmp_loc
+  
+  } else {
+    ## probably filepath - double check
+    if(data_class == 'character'){
+      ## assume filepath
+      if (!file.exists(data)){
+        stop('data is of class character but is not a valid file path')
+      } else {
+        numpreds = ncol(fread(data, nrows = 1L))
+      }
+    } else {
+      stop('Cannot determine class of input data')
+    }  
+  }
+  
+  ## dst arg must exist
+  assert_that(dst, is.notempty(dst))
+  
+  ## check dst is not empty
+  try_dst = paste0(dst, '/', outname)
+  if(file.exists(try_dst)){
+    warning(cat('Destination folder ', outname, ' already exists -\n', 
+                  'a new sub folder will be created in this folder',
+            sep = ''))
+    dst = try_dst
+  } else {
+    dst = try_dst
+    dir.create(dst)
+  }
+  
+  ## check permutations arg
+  assert_that(dst, is.integer(permutations))
+  
+  ## geo 
+  assert_that(dst, is.logical(geo))  
+
+  if (verbose){
+    if (geo){
+      print("Including geographic distance as a predictor")
+    } else {
+      print("Excluding geographic distance as a predictor")
+    } 
+  }
+  
+  ## find dll
+  this_lib = dirname(find.package('gdmToolbox', .libPaths()))
+  dllpath = paste(this_lib, '/gdmToolbox/src/GDM4Rext.dll', sep = '/')
+  assert_that(file.exists(dllpath))
+  ## format path and load
+  dllpath = paste(strsplit(dllpath, '/')[[1]], collapse = '\\')
+  dyn.load(dllpath)
+  
+  ## dll args to write param file
+  wdpath = gsub( "\\\\",  "/", dst)
+  wdpath = gsub('/', '\\\\', wdpath)
+  paramFilePath = paste0(outname, '.txt')
+  datatable = gsub( "\\\\",  "/", data)
+  data = gsub('/', '\\\\', data)
+  numpreds = as.integer(numpreds)
+  do_geo = geo
+  
+  z0 <- .C( "SaveGDMParams", wdpath, paramFilePath, datatable, as.integer(numpreds), as.integer(do_geo))
+  
+  fullparamfilepath <- paste0(wdpath, "\\", paramFilePath)
+  z1 <- .C( "ExtractAndUpdateQuantilesSigTest", fullparamfilepath)
+  
+  z2 <- .C( "DoSigTestGDM", fullparamfilepath, as.integer(iterations))
+  
+  dyn.unload(dllpath)
+  
+  cat('Significance testing done. Output files located here:\n', dst, sep = '')
+  
+  if(load_output){
+    f = list.files(dst, pattern = '.csv')
+    outputs = lapply(f, read.csv)
+    names(outputs) = gsub('.csv', '', unlist(lapply(f, basename)))
+    return(outputs)
+  }
+              
+  
+}
+
+
+#'@title Helper
+#'
+#'@export
+
+is.notempty.string = function(x) {
+  is.string(x) && !is.na(x) && nchar(x)>0
+}
+
